3aefc88db42eb123f487030ee8a31b459c90b408
Michael-Bennett-BYU-I <91813824+Michael-Bennett-BYU-I@users.noreply.github.com>
2021-10-04 21:27:13 -0700

    Michael Bennett's commit message.

--- src/johnston/Main.java
+++ src/johnston/Main.java
@@ -2,5 +2,7 @@ package johnston;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello, my name is Nathan!");
+
+        System.out.println("\nHello Nathan, my name is Michael!");
     }
 }
